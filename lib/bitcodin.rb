b7186d21b0933ef07aa4a49593b37e9a1eab771d
Reinhard Grandl <reinhard.grandl@bitmovin.net>
2015-06-22 12:25:51 +0200

    added EncodingProfile related API calls

--- lib/bitcodin.rb
+++ lib/bitcodin.rb
@@ -6,6 +6,7 @@ require 'bitcodin/input/http_input_config'
 require 'bitcodin/output/s3_output_config'
 require 'bitcodin/output/gcs_output_config'
 require 'bitcodin/output/ftp_output_config'
+require 'bitcodin/media/encoding_profile'
 require 'bitcodin/network/http'
 
 module Bitcodin
@@ -100,16 +101,29 @@ module Bitcodin
 
     # Encoding Profiles
 
-    def createEncodingProfile(encodingProfile)
-
+    def createEncodingProfile(config)
+      url = @apiURL.concat('encoding-profile/create')
+      return @httpClient.sendRequest('post', url, config.values)
     end
 
     def listEncodingProfiles(page = nil)
-
+      url = @apiURL.concat('encoding-profiles')
+      if page.nil?
+        return @httpClient.sendRequest('get', url)
+      else
+        url = url.concat('/').concat(page.to_s)
+        return @httpClient.sendRequest('get', url)
+      end
     end
 
     def getEncodingProfile(id)
+      url = @apiURL.concat('encoding-profile/').concat(id.to_s)
+      return @httpClient.sendRequest('get', url)
+    end
 
+    def deleteEncodingProfile(id)
+      url = @apiURL.concat('encoding-profile/').concat(id.to_s)
+      return @httpClient.sendRequest('delete', url)
     end
 
     # Jobs
