beb4094a810a196fd23365b03bc4c7517fb1d78a
Reinhard Grandl <reinhard.grandl@bitmovin.net>
2015-06-23 09:56:03 +0200

    Created Enum for Job Status

---
+++ lib/bitcodin/job/job_status.rb
@@ -0,0 +1,11 @@
+module Bitcodin
+  class JobStatus
+
+    CREATED     = 'Created'
+    ENQUEUED    = 'Enqueued'
+    IN_PROGRESS = 'In Progress'
+    FINISHED    = 'Finished'
+    ERROR       = 'Error'
+
+  end
+end
