562ad6ffd9d55779277f61c25ccd807ef8bd71fa
Reinhard Grandl <reinhard.grandl@bitmovin.net>
2015-06-23 11:45:41 +0200

    Added simple Input Test Cases

---
+++ test/bitcodin_api_input_tests.rb
@@ -0,0 +1,47 @@
+require 'test/unit'
+require '../lib/bitcodin.rb'
+require 'json'
+
+module Bitcodin
+
+  class ResponseCodes
+
+    GET    = 200
+    POST   = 201
+    DELETE = 204
+    PATCH  = 200
+
+  end
+
+  class BitcodinApiInputTests < Test::Unit::TestCase
+
+    def setup
+      # read access information (e.g. api key, etc.) from file
+      file         = File.read('resources/settings.json')
+      data         = JSON.parse(file)
+      apiKey       = data['apikey']
+
+      # create new bitcodinAPI instance
+      @bitcodinAPI = BitcodinAPI.new(apiKey)
+
+      # create http config
+      @httpConfig = HTTPInputConfig.new('http://ftp.nluug.nl/pub/graphics/blender/demo/movies/Sintel.2010.720p.mkv')
+    end
+
+    def test_createInput
+      response = @bitcodinAPI.createInput(@httpConfig)
+      puts response
+      assert_equal(response.code, ResponseCodes::POST)
+    end
+
+    def test_listAllJobs
+      response = @bitcodinAPI.listAllJobs
+      assert_equal(response.code, ResponseCodes::GET)
+    end
+
+    def teardown
+      ## Nothing up to now
+    end
+
+  end
+end
