8c8da4fb1715f7ddc6bdd8ed893928e5c68fc057
Reinhard Grandl <reinhard.grandl@bitmovin.net>
2015-06-23 10:36:50 +0200

    Added Enum for S3 Regions

---
+++ lib/bitcodin/output/s3_region.rb
@@ -0,0 +1,21 @@
+module Bitcodin
+  class S3Region
+
+    EU_WEST_1    = 'eu-west-1'
+    EU_CENTRAL_1 = 'eu-central-1'
+
+    US_EAST_1     = 'us-east-1'
+    US_WEST_1     = 'us-west-1'
+    US_WEST_2     = 'us-west-2'
+    US_GOV_WEST_1 = 'us-gov-west-1'
+
+    AP_SOUTHEAST_1 = 'ap-southeast-1'
+    AP_SOUTHEAST_2 = 'ap-southeast-2'
+    AP_NORTHEAST_1 = 'ap-northeast-1'
+
+    SA_EAST_1 = 'sa-east-1'
+
+    CA_NORTH_1 = 'ca-north-1'
+
+  end
+end
