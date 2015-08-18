9323318d264ecbe59d1d60ef8f10cf78c28ec566
Dominic Miglar <dominic.miglar@w1r3.net>
2015-08-18 16:33:17 +0200

    improved json "generation"

--- lib/bitcodin/job/job.rb
+++ lib/bitcodin/job/job.rb
@@ -1,39 +1,22 @@
 module Bitcodin
   class Job
-
     attr_accessor :values
-
-    def initialize(inputId, encodingProfileId, manifestTypes, speed=nil, drmConfig=nil)
-      if !drmConfig.nil? && !speed.nil?
-        @values = '{
-          "inputId": '           + inputId.to_s + ',
-          "encodingProfileId": ' + encodingProfileId.to_s + ',
-          "manifestTypes": '     + manifestTypes.to_s + ',
-          "speed": '             + '"' + speed.to_s + '"' + ',
-          "drmConfig": '         + drmConfig + '
-        }'
-      elsif !drmConfig.nil? && speed.nil?
-        @values = '{
-          "inputId": '           + inputId.to_s + ',
-          "encodingProfileId": ' + encodingProfileId.to_s + ',
-          "manifestTypes": '     + manifestTypes.to_s + ',
-          "drmConfig": '         + drmConfig + '
-        }'
-        elsif drmConfig.nil? && !speed.nil?
-          @values = '{
-            "inputId": '           + inputId.to_s + ',
-            "encodingProfileId": ' + encodingProfileId.to_s + ',
-            "manifestTypes": '     + manifestTypes.to_s + ',
-            "speed": '             + speed.to_s + '
-          }'
-        else
-          @values = '{
-            "inputId": '           + inputId.to_s + ',
-            "encodingProfileId": ' + encodingProfileId.to_s + ',
-            "manifestTypes": '     + manifestTypes.to_s + '
-          }'
+    def initialize(inputId, encodingProfileId, manifestTypes, speed=nil, drmConfig=nil, hlsEncryptionConfig=nil)
+      @values = '{'
+      @values += '"inputId": ' + inputId.to_s + ','
+      @values += '"encodingProfileId": ' + encodingProfileId.to_s + ','
+      @values += '"manifestTypes": ' + manifestTypes.to_s + ''
+      if !speed.nil?
+        @values += ',"speed": ' + '"' + speed.to_s + '"' + ''
       end
+      if !drmConfig.nil?
+        @values += ',"drmConfig": ' + drmConfig + ''
+      end
+      if !hlsEncryptionConfig.nil?
+        @values += ',"hlsEncryptionConfig": ' + hlsEncryptionConfig + ''
+      end
+      @values += '}'
+      puts @values
     end
-
   end
 end
