e6aa7394ed402a37d5f879f33f353c8d6826c4ec
Rob Flynn <rob@thingerly.com>
2016-06-16 10:43:05 -0400

    fix subtitle jobs
    - Documentation was incorrect. This fix resolves the inability
      to process VTT mpds/m3u8s

--- lib/bitcodin/subtitle/vtt_hls.rb
+++ lib/bitcodin/subtitle/vtt_hls.rb
@@ -4,7 +4,7 @@ module Bitcodin
 
 		def initialize(jobID, subtitleConfig)
 			@values = '{'
-			@values += '"job": ' + jobID.to_s + ','
+			@values += '"jobId": ' + jobID.to_s + ','
 			@values += '"subtitles": ['
 			@values += subtitleConfig.map(&:values).join(",")
 			@values += ']'
